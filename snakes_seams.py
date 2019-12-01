c6c7e823bca8abbd7bc41761622aaee8fa673006
Mukund Mundhra <mukund8102@gmail.com>
2019-12-01 00:01:49 -0800

    Modifying seams with snakes
    Committer: Mukund Mundhra <mukund8102@gmail.com>
    On branch master
    Your branch is ahead of 'origin/master' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	new file:   snakes_seams.py
    
    Untracked files:
    	.DS_Store
    	out.jpg
    	out2.jpg
    	sc_try.py
    	snakes_try.py

---
+++ snakes_seams.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+
+"""
+Usage: python carver.py <r/c> <scale> <image_in> <image_out>
+Copyright 2018 Karthik Karanth, MIT License
+"""
+
+import sys
+
+from tqdm import trange
+import numpy as np
+from imageio import imread, imwrite
+from scipy.ndimage.filters import convolve
+import cv2
+from skimage.segmentation import active_contour
+
+
+def calc_energy(img):
+    filter_du = np.array([
+        [1.0, 2.0, 1.0],
+        [0.0, 0.0, 0.0],
+        [-1.0, -2.0, -1.0],
+    ])
+    # This converts it from a 2D filter to a 3D filter, replicating the same
+    # filter for each channel: R, G, B
+    filter_du = np.stack([filter_du] * 3, axis=2)
+
+    filter_dv = np.array([
+        [1.0, 0.0, -1.0],
+        [2.0, 0.0, -2.0],
+        [1.0, 0.0, -1.0],
+    ])
+    # This converts it from a 2D filter to a 3D filter, replicating the same
+    # filter for each channel: R, G, B
+    filter_dv = np.stack([filter_dv] * 3, axis=2)
+
+    img = img.astype('float32')
+    convolved = np.absolute(convolve(img, filter_du)) + np.absolute(convolve(img, filter_dv))
+
+    # We sum the energies in the red, green, and blue channels
+    energy_map = convolved.sum(axis=2)
+
+    return energy_map
+
+def crop_c(img, scale_c):
+    r, c, _ = img.shape
+    new_c = int(scale_c * c)
+
+    for i in trange(c - new_c):
+        img = carve_column(img)
+        # cv2.imshow('image',img)
+        # cv2.waitKey(1)
+
+    return img
+
+def crop_r(img, scale_r):
+    img = np.rot90(img, 1, (0, 1))
+    img = crop_c(img, scale_r)
+    img = np.rot90(img, 3, (0, 1))
+    return img
+
+def snake(mask, image):
+    int_mask = (~mask).astype(int)
+    r,c = np.nonzero(int_mask)
+    init = np.array([r,c]).T
+
+    img2 = image.copy()
+    img2[r,c] *= 0
+    cv2.imshow('mask',img2)
+    cv2.waitKey(10)
+
+    snake = active_contour(image, init, bc='fixed', alpha=0.005, beta=10, w_edge=0, gamma=0.01, max_px_move=2)
+    snake = snake.astype(int)
+    # print(snake)
+    snake_mask = np.zeros(mask.shape, dtype=int)
+    snake_mask[snake[:,0], snake[:,1]] = 1
+    img3 = image.copy()
+    img3[snake[:,0], snake[:,1]] *= 0
+    cv2.imshow('snake',img3)
+    cv2.waitKey(10)
+
+
+    return ~snake_mask.astype(bool)
+
+def carve_column(img):
+    r, c, _ = img.shape
+
+    M, backtrack = minimum_seam(img)
+    mask = np.ones((r, c), dtype=np.bool)
+
+    j = np.argmin(M[-1])
+    for i in reversed(range(r)):
+        mask[i, j] = False
+        j = backtrack[i, j]
+
+    # cv2.imshow('mask', mask.astype(np.uint8)*255)
+    # print((~mask).astype(np.uint8)*255)
+    # cv2.waitKey(10)
+
+    mask = snake(mask, img)
+
+    mask = np.stack([mask] * 3, axis=2)
+    img = img[mask].reshape((r, c - 1, 3))
+    return img
+
+def minimum_seam(img):
+    r, c, _ = img.shape
+    energy_map = calc_energy(img)
+
+    M = energy_map.copy()
+    backtrack = np.zeros_like(M, dtype=np.int)
+
+    for i in range(1, r):
+        for j in range(0, c):
+            # Handle the left edge of the image, to ensure we don't index a -1
+            if j == 0:
+                idx = np.argmin(M[i-1, j:j + 2])
+                backtrack[i, j] = idx + j
+                min_energy = M[i-1, idx + j]
+            else:
+                idx = np.argmin(M[i - 1, j - 1:j + 2])
+                backtrack[i, j] = idx + j - 1
+                min_energy = M[i - 1, idx + j - 1]
+
+            M[i, j] += min_energy
+
+    return M, backtrack
+
+def main():
+    if len(sys.argv) != 5:
+        print('usage: carver.py <r/c> <scale> <image_in> <image_out>', file=sys.stderr)
+        sys.exit(1)
+
+    which_axis = sys.argv[1]
+    scale = float(sys.argv[2])
+    in_filename = sys.argv[3]
+    out_filename = sys.argv[4]
+
+    img = imread(in_filename)
+    # img = cv2.resize(img,None,fx=0.2,fy=0.2)
+
+    if which_axis == 'r':
+        out = crop_r(img, scale)
+    elif which_axis == 'c':
+        out = crop_c(img, scale)
+    else:
+        print('usage: carver.py <r/c> <scale> <image_in> <image_out>', file=sys.stderr)
+        sys.exit(1)
+    
+    imwrite(out_filename, out)
+
+if __name__ == '__main__':
+    main()
